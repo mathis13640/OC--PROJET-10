533ada2cff79b1484c3ceda52f7fe02cbbeb7fd5
mathis13640 <73877129+mathis13640@users.noreply.github.com>
2023-10-06 11:57:17 +0200

    implémentation test unitaire janvier-juillet + changement ordre evenements
    (ordre du signe de "<" pour ">" afin que les évènements soient affichés dans l'ordre décroissant)

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,13 +8,10 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
   };
   useEffect(() => {
     nextCard();
--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,27 @@
 /**
- * 
+ *
  */
+import { getMonth } from "./index";
 
 describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
+  describe("When getMonth is called", () => {
+    it("the function return janvier for 2022-01-01 as date", () => {
+      // implémentation test unitaire pour Janvier
+      const date = new Date("2022-01-01");
+      const month = getMonth(date);
+      expect(month).toBe("janvier");
     });
-})
-
+    it("the function return juillet for 2022-07-08 as date", () => {
+      // implémentation test unitaire pour Juillet
+      const date = new Date("2022-07-08");
+      const month = getMonth(date);
+      expect(month).toBe("juillet");
+    });
+    it("the function return undefined for 2022-15-36 as date", () => {
+      // implémentation test unitaire pour une date farfelue
+      const date = new Date("2022-15-36");
+      const month = getMonth(date);
+      expect(month).not.toBeDefined();
+    });
+  });
+});
